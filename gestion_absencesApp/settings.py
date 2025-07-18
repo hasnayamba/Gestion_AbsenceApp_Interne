3fee5754344533df478f46e100816136762a9caf
hasnayamba <yambahssana@gmail.com>
2025-07-18 18:53:59 +0100

    setting_update

--- gestion_absencesApp/settings.py
+++ gestion_absencesApp/settings.py
@@ -20,7 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j1=d+y#1e-)01m=mj1e8m)&-obzgm59y+_k&f3zt=zp^xr_b5@'
+import os
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+if not SECRET_KEY:
+    raise RuntimeError("La variable d'environnement SECRET_KEY n'est pas définie.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
