37998ba0033be0c2ed6d97a9202b1364acb16779
hasnayamba <yambahssana@gmail.com>
2025-07-18 19:07:13 +0100

    deplo_update

--- gestion_absencesApp/deployment.py
+++ gestion_absencesApp/deployment.py
@@ -1,33 +1,30 @@
 import os
-from .settings import *
 from .settings import BASE_DIR
-from .settings import STATIC_ROOT, MEDIA_ROOT
 from urllib.parse import urlparse
 
-# Récupération de la SECRET_KEY
-SECRET_KEY = os.environ.get('SECRET')
+# Récupération de la SECRET_KEY (attention au nom de la variable)
+SECRET_KEY = os.environ.get('SECRET_KEY')
 if not SECRET_KEY:
-    raise RuntimeError("La variable d'environnement SECRET n'est pas définie.")
+    raise RuntimeError("La variable d'environnement SECRET_KEY n'est pas définie.")
 
-# Récupération de l'hôte autorisé pour Azure (ou localhost par défaut)
+# Hôte autorisé pour Azure (ou localhost par défaut)
 WEBSITE_HOSTNAME = os.environ.get('WEBSITE_HOSTNAME', 'localhost')
 ALLOWED_HOSTS = [WEBSITE_HOSTNAME]
 
-# Protection CSRF (nécessaire en production avec Azure)
+# Protection CSRF
 CSRF_TRUSTED_ORIGINS = [f"https://{WEBSITE_HOSTNAME}"]
 
 DEBUG = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # UNE SEULE FOIS ici !
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
@@ -36,10 +33,7 @@ STATIC_URL = '/static/'
 
 connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 
-# Configuration de la base de données pour Azure PostgreSQL
-# Parsing de la chaîne de connexion
 url = urlparse(connection_string)
-
 
 DATABASES = {
     'default': {
